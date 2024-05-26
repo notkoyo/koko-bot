870f8c0eec44d5822abcffb40ecbc972b1c8318b
notkoyo <kaiden01@outlook.de>
2024-05-26 13:09:38 +0100

    fix make teams function

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ const client = new Client({
     IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.GuildMessages,
     IntentsBitField.Flags.MessageContent,
+    IntentsBitField.Flags.GuildVoiceStates,
+    
   ],
 });
 
