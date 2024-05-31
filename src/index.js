5b1b3fabcb3761994a10eb4b8d5c9ae81c7849d5
notkoyo <kaiden01@outlook.de>
2024-05-31 18:45:49 +0100

    refactor intents

--- src/index.js
+++ src/index.js
@@ -1,28 +1,23 @@
-require('dotenv').config();
-const { Client, IntentsBitField } = require('discord.js');
-const eventHandler = require('./handlers/eventHandler');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const {Client, IntentsBitField} = require("discord.js");
+const eventHandler = require("./handlers/eventHandler");
+const mongoose = require("mongoose");
 
-const { TOKEN, MONGODB_URI } = process.env;
+const {TOKEN, MONGODB_URI} = process.env;
+const {Guilds, GuildMembers, GuildMessages, MessageContent, GuildVoiceStates} = IntentsBitField;
 
 const client = new Client({
-  intents: [
-    IntentsBitField.Flags.Guilds,
-    IntentsBitField.Flags.GuildMembers,
-    IntentsBitField.Flags.GuildMessages,
-    IntentsBitField.Flags.MessageContent,
-    IntentsBitField.Flags.GuildVoiceStates,
-  ],
+  intents: [Guilds, GuildMembers, GuildMessages, MessageContent, GuildVoiceStates],
 });
 
 (async () => {
   try {
     await mongoose.connect(MONGODB_URI);
     console.log("Pinged the DB. Successfully connected!");
-    
+
     eventHandler(client);
-  } catch (error) {
-    console.error("Could not connect to DB.");
+  } catch (e) {
+    console.error("Could not connect to DB.", e);
   }
 })();
 
